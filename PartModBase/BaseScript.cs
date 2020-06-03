ccabba18eedaf06e87c411ff1612ab705e84ac9a
Henrique Maziero <henpemaz@gmail.com>
2020-06-03 01:59:33 -0300

    Base Mod

---
+++ PartModBase/BaseScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace PartModHmaz
+{
+    public class BaseScript : MonoBehaviour
+    {
+        public static BaseMod mod;
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
