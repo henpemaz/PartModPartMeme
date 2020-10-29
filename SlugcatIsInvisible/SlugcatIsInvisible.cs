a8a0c51c080d0dee2f4700ec1a749ebf07370a61
Henrique Maziero <henpemaz@gmail.com>
2020-10-29 12:01:55 -0300

    SlugcatIsInvisible mod

---
+++ SlugcatIsInvisible/SlugcatIsInvisible.cs
@@ -0,0 +1,35 @@
+﻿using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SlugcatIsInvisible
+{
+    public class SlugcatIsInvisible : PartialityMod
+    {
+        public SlugcatIsInvisible()
+        {
+            this.ModID = "SlugcatIsInvisible";
+            this.Version = "1.0";
+            this.author = "Henpemaz";
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            On.PlayerGraphics.ApplyPalette += PlayerGraphics_ApplyPalette_hk;
+        }
+
+        public void PlayerGraphics_ApplyPalette_hk(On.PlayerGraphics.orig_ApplyPalette orig, PlayerGraphics instance, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, RoomPalette palette)
+        {
+
+            //return new Color32(0,0,0,0);
+            for (int i = 0; i < sLeaser.sprites.Length; i++)
+            {
+                sLeaser.sprites[i].color = Color.clear;
+            }
+        }
+    }
+}
