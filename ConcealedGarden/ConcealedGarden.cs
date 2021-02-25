d585cf460c2c9eb46e366a4cf14ae12fb17465c1
Henrique Maziero <henpemaz@gmail.com>
2021-02-25 15:20:43 -0300

    Concealed Garden mod initial commit
    cosmetic leaves object

---
+++ ConcealedGarden/ConcealedGarden.cs
@@ -0,0 +1,76 @@
+﻿using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConcealedGarden
+{
+    public class ConcealedGarden : PartialityMod
+    {
+        public ConcealedGarden()
+        {
+            this.ModID = "ConcealedGarden";
+            this.Version = "1.0";
+            this.author = "Henpemaz";
+
+            instance = this;
+        }
+
+        public static ConcealedGarden instance;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+            // Hooking code goose hre
+
+            On.PlacedObject.GenerateEmptyData += PlacedObject_GenerateEmptyData_Patch;
+            On.Room.Loaded += Room_Loaded_Patch;
+            On.DevInterface.ObjectsPage.CreateObjRep += ObjectsPage_CreateObjRep_Patch;
+        }
+
+        public static class EnumExt_ConcealedGarden
+        {
+            public static PlacedObject.Type CosmeticLeaves;
+
+
+        }
+
+        public static void PlacedObject_GenerateEmptyData_Patch(On.PlacedObject.orig_GenerateEmptyData orig, PlacedObject instance)
+        {
+            orig(instance);
+            if (instance.type == EnumExt_ConcealedGarden.CosmeticLeaves)
+            {
+                instance.data = new CosmeticLeaves.CosmeticLeavesObjectData(instance);
+            }
+        }
+
+        private static void ObjectsPage_CreateObjRep_Patch(On.DevInterface.ObjectsPage.orig_CreateObjRep orig, DevInterface.ObjectsPage instance, PlacedObject.Type tp, PlacedObject pObj)
+        {
+            orig(instance, tp, pObj);
+            if (tp == EnumExt_ConcealedGarden.CosmeticLeaves)
+            {
+                DevInterface.PlacedObjectRepresentation old = (DevInterface.PlacedObjectRepresentation)instance.tempNodes.Pop();
+                instance.subNodes.Pop();
+                old.ClearSprites();
+                DevInterface.PlacedObjectRepresentation placedObjectRepresentation = new CosmeticLeaves.CosmeticLeavesObjectRepresentation(instance.owner, tp.ToString() + "_Rep", instance, old.pObj, tp.ToString());
+                instance.tempNodes.Add(placedObjectRepresentation);
+                instance.subNodes.Add(placedObjectRepresentation);
+            }
+        }
+
+        public static void Room_Loaded_Patch(On.Room.orig_Loaded orig, Room instance)
+        {
+            orig(instance);
+
+            for (int l = 0; l < instance.roomSettings.placedObjects.Count; l++)
+            {
+                if (instance.roomSettings.placedObjects[l].active)
+                {
+                    if (instance.roomSettings.placedObjects[l].type == EnumExt_ConcealedGarden.CosmeticLeaves)
+                        instance.AddObject(new CosmeticLeaves(instance.roomSettings.placedObjects[l], instance));
+                }
+            }
+        }
+    }
+}
---
+++ ConcealedGarden/CosmeticLeaves.cs
@@ -0,0 +1,29 @@
+﻿using DevInterface;
+
+namespace ConcealedGarden
+{
+    internal class CosmeticLeaves : UpdatableAndDeletable
+    {
+        private PlacedObject placedObject;
+
+        public CosmeticLeaves(PlacedObject placedObject, Room instance)
+        {
+            this.placedObject = placedObject;
+            this.room = instance;
+        }
+
+        internal class CosmeticLeavesObjectRepresentation : PlacedObjectRepresentation
+        {
+            public CosmeticLeavesObjectRepresentation(DevUI owner, string IDstring, DevUINode parentNode, PlacedObject pObj, string name) : base(owner, IDstring, parentNode, pObj, name)
+            {
+            }
+        }
+
+        internal class CosmeticLeavesObjectData : PlacedObject.Data
+        {
+            public CosmeticLeavesObjectData(PlacedObject owner) : base(owner)
+            {
+            }
+        }
+    }
+}
