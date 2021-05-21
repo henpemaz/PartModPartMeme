37d6c1d5c92b50c752b892a0fe218486494071ca
Henrique Maziero <henpemaz@gmail.com>
2021-05-21 01:31:04 -0300

    Improvements and such to spawncustomizations

--- ConcealedGarden/SpawnCustomizations.cs
+++ ConcealedGarden/SpawnCustomizations.cs
@@ -29,89 +29,107 @@ namespace ConcealedGarden
             On.CicadaAbstractAI.AbstractBehavior += CicadaAbstractAI_AbstractBehavior;
             On.ScavengerAI.Update += ScavengerAI_Update;
             On.ScavengerAbstractAI.AbstractBehavior += ScavengerAbstractAI_AbstractBehavior;
-            On.ScavengerAI.LikeOfPlayer += ScavengerAI_LikeOfPlayer;
+            //On.ScavengerAI.LikeOfPlayer += ScavengerAI_LikeOfPlayer;
+
+            //On.ScavengerGraphics.ctor += ScavengerGraphics_ctor;
         }
 
+        // Scavenger Fried / FollowPlayer support
+        public static class EnumExt_SpawnCustomizations
+        {
+            public static ScavengerAI.Behavior FollowFriend;
+        }
+        private static void ScavengerGraphics_ctor(On.ScavengerGraphics.orig_ctor orig, ScavengerGraphics self, PhysicalObject ow)
+        {
+            self.DEBUGLABELS = new DebugLabel[6];
+            self.DEBUGLABELS[0] = new DebugLabel(ow, new Vector2(0f, 50f));
+            self.DEBUGLABELS[1] = new DebugLabel(ow, new Vector2(0f, 40f));
+            self.DEBUGLABELS[2] = new DebugLabel(ow, new Vector2(0f, 30f));
+            self.DEBUGLABELS[3] = new DebugLabel(ow, new Vector2(0f, 20f));
+            self.DEBUGLABELS[4] = new DebugLabel(ow, new Vector2(0f, 10f));
+            self.DEBUGLABELS[5] = new DebugLabel(ow, new Vector2(0f, 0f));
+            orig(self, ow);
+            //throw new NotImplementedException();
+        }
         private static float ScavengerAI_LikeOfPlayer(On.ScavengerAI.orig_LikeOfPlayer orig, ScavengerAI self, RelationshipTracker.DynamicRelationship dRelation)
         {
             float val = orig(self, dRelation);
-            if (self.friendTracker != null && self.friendTracker.friend.abstractCreature.ID == dRelation.trackerRep.representedCreature.ID)
-            {
-                val += self.friendTracker.Urgency;
-            }
             return Mathf.Clamp(val, 0f, 1f);
         }
         private static void ScavengerAbstractAI_AbstractBehavior(On.ScavengerAbstractAI.orig_AbstractBehavior orig, ScavengerAbstractAI self, int time)
         {
             orig(self, time);
-            //var AbstractCreatureA_AbstractBehaviorI = typeof(AbstractCreatureAI).GetMethod("AbstractBehavior").MethodHandle.GetFunctionPointer();
-            //var baseBehavior = (Action<int>)Activator.CreateInstance(typeof(Action<int>), self, AbstractCreatureA_AbstractBehaviorI);
-            self.MigrationBehavior(0); // time = zero => zero rolls of random migration, just followcreature logic
-        }
-        public static class EnumExt_SpawnCustomizations
-        {
-            public static ScavengerAI.Behavior FollowFriend;
+            if (self.path.Count == 0 && self.squad == null && !(self.bringPearlHome || !self.missionAppropriateGear)) // not doing anything else
+                self.MigrationBehavior(0); // time = zero => zero rolls of random migration, just followcreature logic
         }
         private static void ScavengerAI_Update(On.ScavengerAI.orig_Update orig, ScavengerAI self)
         {
-            orig(self);
-            if (self.friendTracker == null || !self.friendTracker.followClosestFriend) return;
-            AIModule aimodule = self.utilityComparer.HighestUtilityModule();
-            self.currentUtility = self.utilityComparer.HighestUtility();
-            if (aimodule != null)
-            {
-                if (aimodule is FriendTracker && self.currentUtility >= 0.1f)
-                {
-                    self.behavior = EnumExt_SpawnCustomizations.FollowFriend;
-                }
-            }
-            if (self.behavior == EnumExt_SpawnCustomizations.FollowFriend)
-            {
-                if (self.friendTracker.friend == null)
-                {
-                    self.behavior = ScavengerAI.Behavior.Idle;
-                }
-                else
-                {
-                    self.creature.abstractAI.SetDestination(self.friendTracker.friendDest);
-                    self.focusCreature = self.tracker.RepresentationForCreature(self.friendTracker.friend.abstractCreature, false);
-                }
-            }
-            if (self.friendTracker.friend == null)
-            {
-                // Abstract follow-creature logic might do something about it
-                if (UnityEngine.Random.value < 0.02)
-                {
-                    self.creature.abstractAI.AbstractBehavior(1);
-                }
-                if (self.creature.abstractAI.followCreature != null)
-                {
-                    self.friendTracker.friend = self.creature.abstractAI.followCreature.realizedCreature;
-                    self.friendTracker.friendRel = self.creature.state.socialMemory.GetRelationship(self.creature.abstractAI.followCreature.ID);
-                }
-            }
-        }
-        private static void CicadaAI_Update(On.CicadaAI.orig_Update orig, CicadaAI self)
-        {
-            // reordered to prevent IDLE from taking precedence
-            if (self.friendTracker != null && self.friendTracker.followClosestFriend)
+            // we're running a frame late but its best to have our code run first here and then let the AI overwrite behavior
+            if (self.friendTracker != null && self.friendTracker.followClosestFriend) // {follow} switch
             {
                 AIModule aimodule = self.utilityComparer.HighestUtilityModule();
                 self.currentUtility = self.utilityComparer.HighestUtility();
                 if (aimodule != null)
                 {
-                    if (aimodule is FriendTracker && self.currentUtility >= 0.1f)
+                    if (aimodule is FriendTracker && self.currentUtility > 0.2f)
                     {
-                        self.behavior = CicadaAI.Behavior.FollowFriend;
+                        if (!(self.scavenger.abstractCreature.abstractAI as ScavengerAbstractAI).bringPearlHome)
+                        {
+                            self.behavior = EnumExt_SpawnCustomizations.FollowFriend;
+                        }
+                        else // got pearl, gotta dip
+                        {
+                            // this causes tracker urgency to dip when its called next during the actual AI code
+                            self.behavior = ScavengerAI.Behavior.Travel;
+                            self.friendTracker.friendRel = null; // Don't worry about me I'll be fine you go
+                        }
+                    }
+                }
+                if (self.behavior == EnumExt_SpawnCustomizations.FollowFriend)
+                {
+                    if (self.friendTracker.friend != null)
+                    {
+                        self.creature.abstractAI.SetDestination(self.friendTracker.friendDest);
+                        self.focusCreature = self.tracker.RepresentationForCreature(self.friendTracker.friend.abstractCreature, false);
+                    }
+                }
+                if (self.friendTracker.friend == null) // not found friend in room yet
+                {
+                    if (self.creature.abstractAI.followCreature != null) // but maybe abstai did
+                    {
+                        self.friendTracker.friend = self.creature.abstractAI.followCreature.realizedCreature;
+                        self.friendTracker.friendRel = self.creature.state.socialMemory.GetRelationship(self.creature.abstractAI.followCreature.ID);
+                    }
+                }
+            }
+            orig(self);
+        }
+
+        // Cicada FollowPlayer support
+        private static void CicadaAI_Update(On.CicadaAI.orig_Update orig, CicadaAI self)
+        {
+            // reordered to prevent IDLE from taking precedence
+            if (self.friendTracker != null && self.friendTracker.followClosestFriend) // {follow} switch
+            {
+                //if (!self.cicada.Consious) // Fricking hell spaghetti code joar fix your damn game
+                //{
+                //    self.cicada.LoseAllGrasps();
+                //}
+                AIModule aimodule = self.utilityComparer.HighestUtilityModule();
+                self.currentUtility = self.utilityComparer.HighestUtility();
+                if (aimodule != null)
+                {
+                    if (aimodule is FriendTracker && self.currentUtility > 0.2f) // 0.2 = min, same room within 10 tiles
+                    {
+                        if (self.behavior != CicadaAI.Behavior.ReturnPrey || self.cicada.grasps[0] == null)
+                        {
+                            self.behavior = CicadaAI.Behavior.FollowFriend;
+                        }
                     }
                 }
                 if (self.behavior == CicadaAI.Behavior.FollowFriend)
                 {
-                    if (self.friendTracker.friend == null)
-                    {
-                        self.behavior = CicadaAI.Behavior.Idle;
-                    }
-                    else
+                    if (self.friendTracker.friend != null)
                     {
                         self.creature.abstractAI.SetDestination(self.friendTracker.friendDest);
                         self.focusCreature = self.tracker.RepresentationForCreature(self.friendTracker.friend.abstractCreature, false);
@@ -119,11 +137,6 @@ namespace ConcealedGarden
                 }
                 if (self.friendTracker.friend == null)
                 {
-                    // Abstract follow-creature logic might do something about it
-                    if (UnityEngine.Random.value < 0.02)
-                    {
-                        self.creature.abstractAI.AbstractBehavior(1);
-                    }
                     if (self.creature.abstractAI.followCreature != null)
                     {
                         self.friendTracker.friend = self.creature.abstractAI.followCreature.realizedCreature;
@@ -135,13 +148,12 @@ namespace ConcealedGarden
         }
         private static void CicadaAbstractAI_AbstractBehavior(On.CicadaAbstractAI.orig_AbstractBehavior orig, CicadaAbstractAI self, int time)
         {
-            //if (self.followCreature == null)
             orig(self, time);
-            //// I don't think I can call this early and store it because of JIT ?
-            //var AbstractCreatureA_AbstractBehaviorI = typeof(AbstractCreatureAI).GetMethod("AbstractBehavior").MethodHandle.GetFunctionPointer();
-            //var baseBehavior = (Action<int>)Activator.CreateInstance(typeof(Action<int>), self, AbstractCreatureA_AbstractBehaviorI);
-            self.MigrationBehavior(0); // time = zero => zero rolls of random migration, just followcreature logic
+            if (self.path.Count == 0) // not currently going anywhere
+                self.MigrationBehavior(0); // time = zero => zero rolls of random migration, just followcreature logic
         }
+
+        // Add friendtracker and configure follow-ness for supported creature types
         private static void AbstractCreature_InitiateAI(On.AbstractCreature.orig_InitiateAI orig, AbstractCreature self)
         {
             orig(self);
@@ -163,7 +175,7 @@ namespace ConcealedGarden
                         if (text == "friend")
                         {
                             if (self.abstractAI.RealAI.friendTracker == null) self.abstractAI.RealAI.AddModule(new FriendTracker(self.abstractAI.RealAI));
-                            Debug.LogError("Friend module added in " + self);
+                            Debug.Log("Friend module added in " + self);
                         }
 
                         if (text == "follow")
@@ -171,25 +183,25 @@ namespace ConcealedGarden
                             if (self.abstractAI.RealAI.friendTracker != null && self.abstractAI.RealAI.utilityComparer != null)
                             {
                                 self.abstractAI.RealAI.friendTracker.followClosestFriend = true;
-                                bool tracked = false;
-                                foreach (var tracker in self.abstractAI.RealAI.utilityComparer.uTrackers)
-                                {
-                                    if (tracker.module == self.abstractAI.RealAI.friendTracker) tracked = true;
-                                }
-                                if (!tracked) self.abstractAI.RealAI.utilityComparer.AddComparedModule(self.abstractAI.RealAI.friendTracker, null, array2.Length > 1 ? float.Parse(array2[1]) : 0.8f, 1.2f);
-                                Debug.LogError("Follow configured in " + self);
+                                UtilityComparer.UtilityTracker utracker = self.abstractAI.RealAI.utilityComparer.GetUtilityTracker(self.abstractAI.RealAI.friendTracker);
+                                if (utracker == null) self.abstractAI.RealAI.utilityComparer.AddComparedModule(self.abstractAI.RealAI.friendTracker, null, array2.Length > 1 ? float.Parse(array2[1]) : 0.8f, 1.2f);
+                                Debug.Log("Follow configured in " + self);
                             }
                         }
                     }
                 }
             }
         }
+        // Store a reference to the current WorldLoader because several things being created need a reference to their spawners before spawnData is attributed to the creature
         private static WeakReference currentWorldLoader;
         private static void WorldLoader_ctor(On.WorldLoader.orig_ctor orig, WorldLoader self, RainWorldGame game, int playerCharacter, bool singleRoomWorld, string worldName, Region region, RainWorldGame.SetupValues setupValues)
         {
             currentWorldLoader = new WeakReference(self);
             orig(self, game, playerCharacter, singleRoomWorld, worldName, region, setupValues);
         }
+
+        // Load ID into EntityID right before AbstractCreature_ctor
+        // uses spawndata from worldloader because its normally only added after AbstractCreature_ctor
         private static EntityID RainWorldGame_GetNewID_1(On.RainWorldGame.orig_GetNewID_1 orig, RainWorldGame self, int spawner)
         {
             EntityID id = orig(self, spawner);
@@ -203,8 +215,7 @@ namespace ConcealedGarden
                 {
                     // game.overWorld isn't set until the constructor is done so we can't use that reference
                     // Overworld.LoadWorld doesn't set a reference to worldloader anywhere while its doing its thing :/
-                    WorldLoader worldLoader = currentWorldLoader.Target as WorldLoader;
-                    if (worldLoader != null && !worldLoader.Finished && worldLoader.world.region != null && worldLoader.world.region.regionNumber == region)
+                    if (currentWorldLoader.Target is WorldLoader worldLoader && !worldLoader.Finished && worldLoader.world.region != null && worldLoader.world.region.regionNumber == region)
                     {
                         if (worldLoader.world.spawners[inregionspawn] is World.SimpleSpawner simpleSpawner)
                         {
@@ -239,6 +250,7 @@ namespace ConcealedGarden
             // also arena uses the paramless ctor
             return id;
         }
+        // uses spawndata from scavenger to decide on which trader to assign
         private static float Trader_ScavScore(On.ScavengersWorldAI.Trader.orig_ScavScore orig, ScavengersWorldAI.Trader self, ScavengerAbstractAI testScav)
         {
             float score = orig(self, testScav);
@@ -280,12 +292,13 @@ namespace ConcealedGarden
             if (testScav.squad != null && testScav.squad.missionType == ScavengerAbstractAI.ScavengerSquad.MissionID.Trade && !specificallyAssigned) return 0f; // Already assigned
             return score;
         }
+        // Load relationships into CreatureState during AbstractCreature_ctor
+        // uses spawndata from worldloader because its normally only added after AbstractCreature_ctor
         private static void AbstractCreature_ctor(On.AbstractCreature.orig_ctor orig, AbstractCreature self, World world, CreatureTemplate creatureTemplate, Creature realizedCreature, WorldCoordinate pos, EntityID ID)
         {
             orig(self, world, creatureTemplate, realizedCreature, pos, ID);
-            WorldLoader worldLoader = currentWorldLoader.Target as WorldLoader;
 
-            if (worldLoader != null && ID.spawner >= 0 && worldLoader.game.IsStorySession) // called juuuust from WorldLoader.GeneratePopulation most likely, lets play safe though
+            if (currentWorldLoader.Target is WorldLoader worldLoader && ID.spawner >= 0 && worldLoader.game.IsStorySession) // called juuuust from WorldLoader.GeneratePopulation most likely, lets play safe though
             {
                 int region = UnityEngine.Mathf.FloorToInt(ID.spawner / 1000f);
                 int inregionspawn = ID.spawner - region * 1000;
@@ -343,6 +356,8 @@ namespace ConcealedGarden
                 catch { UnityEngine.Debug.LogError("ConcealedGarden: Something terrible happened while trying to parse traits for spawner " + ID.spawner + " " + spawnData); }
             }
         }
+        // Load relationships into CreatureState during CreatureState_LoadFromString
+        // uses spawndata from the string being loaded
         private static void CreatureState_LoadFromString(On.CreatureState.orig_LoadFromString orig, CreatureState self, string[] s)
         {
             if (self.socialMemory != null && self.socialMemory.relationShips.Count > 0) self.socialMemory.relationShips.Clear();
@@ -389,6 +404,8 @@ namespace ConcealedGarden
             }
             catch { UnityEngine.Debug.LogError("ConcealedGarden: Something terrible happened while trying to parse traits from state as " + self.creature.ID.spawner + " at CreatureState_LoadFromString " + spawnData); }
         }
+        // Load relationships into CreatureState during AbstractCreature_ctor
+        // uses spawndata from worldloader because its normally only added after AbstractCreature_ctor
         private static void CreatureState_ctor(On.CreatureState.orig_ctor orig, CreatureState self, AbstractCreature creature)
         {
             orig(self, creature);
