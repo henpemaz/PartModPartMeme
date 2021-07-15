db6b93144fcfac2da548284257690a50da3ff730
Henrique Maziero <henpemaz@gmail.com>
2021-07-15 01:41:18 -0300

    SilentEchos mod

---
+++ SilentEchos/SilentEchosMod.cs
@@ -0,0 +1,31 @@
+﻿using Partiality.Modloader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SilentEchos
+{
+    public class SilentEchosMod : PartialityMod
+    {
+        public SilentEchosMod()
+        {
+            this.ModID = "SilentEchosMod";
+            this.Version = "1.0";
+            this.author = "Henpemaz";
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            On.Ghost.Update += Ghost_Update;
+        }
+
+        private void Ghost_Update(On.Ghost.orig_Update orig, Ghost self, bool eu)
+        {
+            orig(self, eu);
+            self.onScreenCounter = UnityEngine.Mathf.Min(self.onScreenCounter, 1);
+        }
+    }
+}
