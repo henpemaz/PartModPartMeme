bd21a209477fb63fa9ff2e718e0a317f8856ff5c
Henrique Maziero <henpemaz@gmail.com>
2021-10-04 00:51:05 -0300

    CG things

--- ConcealedGarden/CGQuickAndDirtyFix.cs
+++ ConcealedGarden/CGQuickAndDirtyFix.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace ConcealedGarden
 {
+    /// <summary>
+    /// Fixes weird things from Climbables, temporary solution until I decide how I want to handle an update for it :monke:
+    /// </summary>
     internal class CGQuickAndDirtyFix : UpdatableAndDeletable, IDrawable
     {
         public CGQuickAndDirtyFix(Room room)
@@ -13,6 +16,21 @@ namespace ConcealedGarden
         internal static void Register()
         {
             PlacedObjectsManager.RegisterManagedObject(new PlacedObjectsManager.ManagedObjectType("CGQuickAndDirtyFix", typeof(CGQuickAndDirtyFix), null, null));
+            On.Player.MovementUpdate += Player_MovementUpdate;
+        }
+
+        private static void Player_MovementUpdate(On.Player.orig_MovementUpdate orig, Player self, bool eu)
+        {
+            Player.BodyModeIndex prevMode = self.bodyMode;
+            orig(self, eu);
+            if (prevMode == Player.BodyModeIndex.CorridorClimb && self.bodyMode == Player.BodyModeIndex.Default)
+            {
+                ClimbableVinesSystem.VinePosition vinePosition2 = self.room.climbableVines.VineOverlap(self.mainBodyChunk.pos, self.mainBodyChunk.rad);
+                if (vinePosition2 != null)
+                {
+                    self.wantToGrab = 1; // let grab naturally next frame
+                }
+            }
         }
 
         public void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
