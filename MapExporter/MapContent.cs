3cb7c0003f4fad664dd6aa3dc31b232908356366
Henrique Maziero <henpemaz@gmail.com>
2021-11-18 13:17:43 -0300

    Map exported embetterning

---
+++ MapExporter/MapContent.cs
@@ -0,0 +1,293 @@
+﻿using System.Collections.Generic;
+using System.Security;
+using System.Security.Permissions;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System;
+using System.Linq;
+using System.IO;
+using RWCustom;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace MapExporter
+{
+    public class MapContent : LizardSkin.IJsonSerializable
+    {
+        private readonly Dictionary<string, RoomEntry> rooms;
+        private readonly List<ConnectionEntry> connections;
+        private readonly string name;
+        private readonly string acronym;
+
+        public MapContent(World world)
+        {
+            acronym = world.name;
+            name = NameOfRegion(world);
+
+            rooms = (from r in world.abstractRooms select new RoomEntry(r.name)).ToDictionary(e => e.roomName, e => e);
+            connections = new List<ConnectionEntry>();
+
+            fgcolors = new List<Color>();
+            bgcolors = new List<Color>();
+
+            DevInterface.DevUI fakeDevUi = System.Runtime.Serialization.FormatterServices.GetUninitializedObject(typeof(DevInterface.DevUI)) as DevInterface.DevUI;
+            DevInterface.MapPage fakeMapPage = System.Runtime.Serialization.FormatterServices.GetUninitializedObject(typeof(DevInterface.MapPage)) as DevInterface.MapPage;
+            fakeMapPage.owner = fakeDevUi;
+            fakeDevUi.game = world.game;
+            fakeMapPage.filePath = string.Concat(new object[]
+            {
+                    RWCustom.Custom.RootFolderDirectory(),
+                    "World",
+                    Path.DirectorySeparatorChar,
+                    "Regions",
+                    Path.DirectorySeparatorChar,
+                    world.name,
+                    Path.DirectorySeparatorChar,
+                    "map_",
+                    world.name,
+                    ".txt"
+            });
+
+            LoadMapConfig(fakeMapPage);
+
+            Type.GetType("CustomRegions.DevInterface.MapPageHook, CustomRegions")
+                .GetMethod("MapPage_LoadMapConfig", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Static)
+                .Invoke(null, new object[] {
+                        new On.DevInterface.MapPage.orig_LoadMapConfig(LoadMapConfig),
+                        fakeMapPage });
+
+            fakeDevUi.game = null;
+            fakeMapPage.owner = null;
+
+            foreach (var room in rooms)
+            {
+                if(!room.Value.everParsed) { Debug.Log("Room " + room.Key + " doesn't have map data"); Debug.LogError("Room " + room.Key + " doesn't have map data"); }
+            }
+        }
+
+        static float[] vec2arr(Vector2 vec) => new float[] { vec.x, vec.y };
+        static float[] vec2arr(Vector3 vec) => new float[] { vec.x, vec.y, vec.z };
+
+        public void UpdateRoom(Room room)
+        {
+            rooms[room.abstractRoom.name].UpdateEntry(room);
+        }
+
+        class RoomEntry : LizardSkin.IJsonSerializable
+        {
+            public string roomName;
+
+            public RoomEntry(string roomName)
+            {
+                this.roomName = roomName;
+            }
+            // from map txt
+            public Vector2 devPos;
+            public Vector2 canPos;
+            public int canLayer;
+            public int subregion;
+            public bool everParsed = false;
+            public void ParseEntry(string line)
+            {
+                //Debug.Log(line);
+                string[] arr = Regex.Split(line, ": ");
+                if (roomName != arr[0]) throw new Exception();
+                string[] arr2 = Regex.Split(arr[1], ",");
+                canPos.x = float.Parse(arr2[0]);
+                canPos.y = float.Parse(arr2[1]);
+                devPos.x = float.Parse(arr2[2]);
+                devPos.y = float.Parse(arr2[3]);
+                canLayer = int.Parse(arr2[4]);
+                subregion = int.Parse(arr2[5]);
+                everParsed = true;
+            }
+
+            // from room
+            public Vector2[] cameras;
+            private int[] size;
+
+            public void UpdateEntry(Room room)
+            {
+                cameras = room.cameraPositions;
+
+                this.size = new int[] { room.Width, room.Height };
+            }
+
+            // wish there was a better way to do this
+            public Dictionary<string, object> ToJson()
+            {
+                return new Dictionary<string, object>()
+                    {
+                        { "roomName", roomName },
+                        { "canPos", vec2arr(canPos) },
+                        { "canLayer", canLayer },
+                        { "devPos", vec2arr(devPos) },
+                        { "subregion", subregion },
+                        { "cameras", cameras != null ? (from c in cameras select vec2arr(c)).ToArray() : null},
+                    { "size", size}
+                    };
+            }
+        }
+
+
+        class ConnectionEntry : LizardSkin.IJsonSerializable
+        {
+            public string roomA;
+            public string roomB;
+            public IntVector2 posA;
+            public IntVector2 posB;
+            public int dirA;
+            public int dirB;
+
+            public ConnectionEntry(string line)
+            {
+                string[] stuff = Regex.Split(Regex.Split(line, ": ")[1], ",");
+                this.roomA = stuff[0];
+                this.roomB = stuff[1];
+                this.posA = new IntVector2(int.Parse(stuff[2]), int.Parse(stuff[3]));
+                this.posB = new IntVector2(int.Parse(stuff[4]), int.Parse(stuff[5]));
+                this.dirA = int.Parse(stuff[6]);
+                this.dirB = int.Parse(stuff[7]);
+            }
+
+            public Dictionary<string, object> ToJson()
+            {
+                return new Dictionary<string, object>()
+                {
+                    { "roomA", roomA },
+                    { "roomB", roomB },
+                    { "posA", posA },
+                    { "posB", posB },
+                    { "dirA", dirA },
+                    { "dirB", dirB },
+                };
+            }
+        }
+
+        public void LoadMapConfig(DevInterface.MapPage fakeMapPage)
+        {
+            if (!File.Exists(fakeMapPage.filePath)) return;
+            Debug.Log("reading map file: " + fakeMapPage.filePath);
+            string[] contents = File.ReadAllLines(fakeMapPage.filePath);
+            foreach (var s in contents)
+            {
+                string sname = Regex.Split(s, ": ")[0];
+                if (rooms.TryGetValue(sname, out RoomEntry room)) room.ParseEntry(s);
+                if (sname == "Connection") this.connections.Add(new ConnectionEntry(s));
+            }
+        }
+
+        private string NameOfRegion(World world)
+        {
+            string text = null;
+            // copypaste from game
+            switch (world.name)
+            {
+                case "CC":
+                    text = "Chimney Canopy";
+                    break;
+                case "DS":
+                    text = "Drainage System";
+                    break;
+                case "HI":
+                    text = "Industrial Complex";
+                    break;
+                case "GW":
+                    text = "Garbage Wastes";
+                    break;
+                case "SI":
+                    text = "Sky Islands";
+                    break;
+                case "SU":
+                    text = "Outskirts";
+                    break;
+                case "SH":
+                    text = "Shaded Citadel";
+                    break;
+                case "SL":
+                    text = "Shoreline";
+                    break;
+                case "LF":
+                    text = "Farm Arrays";
+                    break;
+                case "UW":
+                    text = "The Exterior";
+                    break;
+                case "SB":
+                    text = "Subterranean";
+                    break;
+                case "SS":
+                    text = "Five Pebbles";
+                    break;
+            }
+
+            // copypaste from csr
+            string regID = world.name;
+            bool customRegion = true;
+            var vanillaRegions = CustomRegions.Mod.CustomWorldMod.VanillaRegions();
+            for (int i = 0; i < vanillaRegions.Length; i++)
+            {
+                if (regID == vanillaRegions[i])
+                {
+                    customRegion = false;
+                }
+            }
+            if (customRegion)
+            {
+                string fullRegionName = "N / A";
+                //CustomWorldMod.activatedPacks.TryGetValue(text2, out fullRegionName);
+                if (CustomRegions.Mod.CustomWorldMod.activeModdedRegions.Contains(regID))
+                {
+                    foreach (KeyValuePair<string, string> entry in CustomRegions.Mod.CustomWorldMod.activatedPacks)
+                    {
+                        if (CustomRegions.Mod.CustomWorldMod.installedPacks[entry.Key].regions.Contains(regID))
+                        {
+                            string regionName = (string)Type.GetType("CustomRegions.CWorld.RegionHook, CustomRegions")
+                                .GetMethod("GetSubRegionName", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Static)
+                                .Invoke(null, new object[] {
+                                        entry.Value,
+                                        regID });
+                            //string regionName = CustomRegions.CWorld.RegionHook.GetSubRegionName(entry.Value, regID);
+                            if (CustomRegions.Mod.CustomWorldMod.installedPacks[entry.Key].useRegionName && regionName != null)
+                            {
+                                fullRegionName = regionName;
+                            }
+                            else
+                            {
+                                fullRegionName = entry.Key;
+                            }
+                            break;
+                        }
+                    }
+                }
+                text = fullRegionName;
+            }
+            return text;
+        }
+
+        public Dictionary<string, object> ToJson()
+        {
+            return new Dictionary<string, object>()
+            {
+                { "name", name },
+                { "acronym", acronym },
+                { "rooms", rooms },
+                { "connections", connections },
+                { "fgcolors" , (from s in fgcolors select  vec2arr((Vector3)(Vector4)s)).ToList()},
+                { "bgcolors" , (from s in bgcolors select  vec2arr((Vector3)(Vector4)s)).ToList()},
+            };
+        }
+
+        internal void LogPalette(RoomPalette currentPalette)
+        {
+            // get sky color and fg color (px 00 and 07)
+            Color fg = currentPalette.texture.GetPixel(0, 0);
+            Color bg = currentPalette.texture.GetPixel(0, 7);
+            this.fgcolors.Add(fg);
+            this.bgcolors.Add(bg);
+        }
+
+        List<Color> fgcolors;
+        List<Color> bgcolors;
+    }
+}
--- MapExporter/MapExporter.cs
+++ MapExporter/MapExporter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Security;
@@ -14,7 +13,7 @@ using MonoMod.RuntimeDetour;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 namespace MapExporter
 {
-    public class MapExporter : PartialityMod
+    public partial class MapExporter : PartialityMod
     {
         public MapExporter()
         {
@@ -38,63 +37,72 @@ namespace MapExporter
         {
             On.RainWorld.LoadSetupValues += RainWorld_LoadSetupValues;
             On.World.SpawnGhost += World_SpawnGhost;
-
             On.RainWorldGame.ctor += RainWorldGame_ctor;
             On.RainWorldGame.Update += RainWorldGame_Update;
             On.RainWorldGame.RawUpdate += RainWorldGame_RawUpdate;
             new Hook(typeof(RainWorldGame).GetProperty("TimeSpeedFac").GetGetMethod(), typeof(MapExporter).GetMethod("RainWorldGame_ZeroProperty"), this);
             new Hook(typeof(RainWorldGame).GetProperty("InitialBlackSeconds").GetGetMethod(), typeof(MapExporter).GetMethod("RainWorldGame_ZeroProperty"), this);
             new Hook(typeof(RainWorldGame).GetProperty("FadeInTime").GetGetMethod(), typeof(MapExporter).GetMethod("RainWorldGame_ZeroProperty"), this);
-
             On.OverWorld.WorldLoaded += OverWorld_WorldLoaded;
-
             On.Room.ReadyForAI += Room_ReadyForAI;
             On.Room.Loaded += Room_Loaded;
             On.Room.ScreenMovement += Room_ScreenMovement;
-
             On.RoomCamera.DrawUpdate += RoomCamera_DrawUpdate;
-
             On.VoidSpawnGraphics.DrawSprites += VoidSpawnGraphics_DrawSprites;
-
             On.AntiGravity.BrokenAntiGravity.ctor += BrokenAntiGravity_ctor;
 
             orig(self);
         }
-
+        #region fixes
+        // shortcut consistency
         private void RoomCamera_DrawUpdate(On.RoomCamera.orig_DrawUpdate orig, RoomCamera self, float timeStacker, float timeSpeed)
         {
-            if(self.room != null && self.room.shortcutsBlinking != null) self.room.shortcutsBlinking = new float[self.room.shortcuts.Length, 4];
+            if (self.room != null && self.room.shortcutsBlinking != null)
+            {
+                self.room.shortcutsBlinking = new float[self.room.shortcuts.Length, 4];
+                for (int i = 0; i < self.room.shortcutsBlinking.GetLength(0); i++)
+                {
+                    self.room.shortcutsBlinking[i, 3] = -1;
+                }
+            }
             orig(self, timeStacker, timeSpeed);
         }
-
+        // no shake
         private void Room_ScreenMovement(On.Room.orig_ScreenMovement orig, Room self, Vector2? pos, Vector2 bump, float shake)
         {
             return;
         }
-        
+        // update faster
         private void RainWorldGame_RawUpdate(On.RainWorldGame.orig_RawUpdate orig, RainWorldGame self, float dt)
         {
             self.myTimeStacker += 2f;
             orig(self, dt);
         }
-
+        //  no grav swithcing
         private void BrokenAntiGravity_ctor(On.AntiGravity.BrokenAntiGravity.orig_ctor orig, AntiGravity.BrokenAntiGravity self, int cycleMin, int cycleMax, RainWorldGame game)
         {
             orig(self, cycleMin, cycleMax, game);
+            self.on = false;
+            self.from = ((!self.on) ? 0f : 1f);
+            self.to = ((!self.on) ? 0f : 1f);
+            self.lights = self.to;
             self.counter = 40000;
         }
 
+        // zeroes some annoying fades
         public delegate float orig_PropertyToZero(RainWorldGame self);
         public float RainWorldGame_ZeroProperty(orig_PropertyToZero orig, RainWorldGame self)
         {
             return 0f;
         }
 
+        // no ghosts
         private void World_SpawnGhost(On.World.orig_SpawnGhost orig, World self)
         {
             return;
         }
 
+        // setup == useful
         private RainWorldGame.SetupValues RainWorld_LoadSetupValues(On.RainWorld.orig_LoadSetupValues orig, bool distributionBuild)
         {
             var setup = orig(false);
@@ -124,6 +132,7 @@ namespace MapExporter
             return setup;
         }
 
+        // fuck you in particular
         private void VoidSpawnGraphics_DrawSprites(On.VoidSpawnGraphics.orig_DrawSprites orig, VoidSpawnGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
         {
             //youre code bad
@@ -133,6 +142,7 @@ namespace MapExporter
             }
         }
 
+        // effects blacklist
         private void Room_Loaded(On.Room.orig_Loaded orig, Room self)
         {
             for (int i = self.roomSettings.effects.Count - 1; i >= 0; i--)
@@ -142,6 +152,7 @@ namespace MapExporter
             orig(self);
         }
 
+        // no orcacles
         private void Room_ReadyForAI(On.Room.orig_ReadyForAI orig, Room self)
         {
             string oldname = self.abstractRoom.name;
@@ -150,22 +161,30 @@ namespace MapExporter
             self.abstractRoom.name = oldname;
         }
 
+        // no gate switching
         private void OverWorld_WorldLoaded(On.OverWorld.orig_WorldLoaded orig, OverWorld self)
         {
             return; // orig assumes a gate
         }
 
+        #endregion fixes
+
+        // start
         private void RainWorldGame_ctor(On.RainWorldGame.orig_ctor orig, RainWorldGame self, ProcessManager manager)
         {
             orig(self, manager);
+            // No players
             self.session.Players[0].Room.RemoveEntity(self.session.Players[0]);
             self.session.Players.Clear();
             self.cameras[0].followAbstractCreature = null;
             self.roomRealizer.followCreature = null;
             self.roomRealizer = null;
 
+            // misc wtf fixes
             self.GetStorySession.saveState.theGlow = false;
             self.rainWorld.setup.playerGlowing = false;
+
+            // no tutorials (cause nullrefs)
             self.GetStorySession.saveState.deathPersistentSaveData.KarmaFlowerMessage = true;
             self.GetStorySession.saveState.deathPersistentSaveData.ScavMerchantMessage = true;
             self.GetStorySession.saveState.deathPersistentSaveData.ScavTollMessage = true;
@@ -185,14 +204,14 @@ namespace MapExporter
 
         string PathOfRegion(string region)
         {
-            string path = Custom.RootFolderDirectory() + "Export" + Path.DirectorySeparatorChar + region;
+            string path = Custom.RootFolderDirectory() + "Export" + Path.DirectorySeparatorChar + region + Path.DirectorySeparatorChar;
             Directory.CreateDirectory(path);
             return path;
         }
 
         string PathOfRoom(string region, string roomname)
         {
-            return PathOfRegion(region) + Path.DirectorySeparatorChar + roomname;
+            return PathOfRegion(region) + roomname;
         }
 
         string PathOfScreenshot(string region, string roomname, int num)
@@ -200,23 +219,34 @@ namespace MapExporter
             return PathOfRoom(region, roomname) + "_" + num.ToString() + ".png";
         }
 
+        // config
+        string skipTo = null;
+        bool oneRegion = false;
+        bool exportScreenshots = true;
+
+        // Runs half-synchronously to the game loop, bless iters
         System.Collections.IEnumerator captureTask;
         private System.Collections.IEnumerator CaptureTask(RainWorldGame game)
         {
+            // Task start
             Debug.Log("capture task start");
-            // 1st camera transition is a bit whack ? give it a sec
+            // 1st camera transition is a bit whack ? give it a sec to load
             //while (game.cameras[0].www != null) yield return null;
             while (game.cameras[0].room == null || !game.cameras[0].room.ReadyForPlayer) yield return null;
-            for (int i = 0; i < 30; i++) yield return null;
+            for (int i = 0; i < 40; i++) yield return null;
+            // ok game loaded I suppose
 
-            bool skipped = true;
+            // Iterate over each region
+            bool skipped = skipTo == null;
             foreach (var regionstr in Menu.FastTravelScreen.GetRegionOrder())
             {
-                if(!skipped && regionstr != "LP")
+                // debug, skip to a region
+                if(!skipped && regionstr != skipTo)
                 {
                     continue;
                 } skipped = true;
 
+                // load region
                 Debug.Log("capture task entering " + regionstr);
                 if (game.overWorld.activeWorld == null || game.overWorld.activeWorld.region.name != regionstr)
                 {
@@ -225,16 +255,23 @@ namespace MapExporter
                     Debug.Log("capture task loaded " + regionstr);
                 }
 
-                foreach(var room in game.overWorld.activeWorld.abstractRooms)
+                MapContent mapContent = new MapContent(game.world);
+                
+                // Iterate over each room
+                foreach (var room in game.overWorld.activeWorld.abstractRooms)
                 {
+                    //catcha
                     if (room.offScreenDen) continue;
                     Debug.Log("capture task room " + room.name);
+                    // load room
                     game.overWorld.activeWorld.loadingRooms.Clear();
                     game.overWorld.activeWorld.ActivateRoom(room);
+                    // load room until it is loaded
                     if(game.overWorld.activeWorld.loadingRooms.Count > 0 && game.overWorld.activeWorld.loadingRooms[0].room == room.realizedRoom)
                     {
                         RoomPreparer loading = game.overWorld.activeWorld.loadingRooms[0];
                         yield return null;
+                        // not too sure how each of these states work so just uuuh bruteforce
                         while (!room.realizedRoom.ReadyForPlayer)
                         {
                             if (!loading.done)
@@ -248,23 +285,40 @@ namespace MapExporter
                             yield return null;
                         }
                     }
+
+                    // go to room
                     game.cameras[0].MoveCamera(room.realizedRoom, 0);
                     game.cameras[0].virtualMicrophone.AllQuiet();
+                    // get to room
                     while(game.cameras[0].loadingRoom != null) yield return null;
+
+                    mapContent.UpdateRoom(room.realizedRoom);
+
+                    // on each camera
                     for (int i = 0; i < room.realizedRoom.cameraPositions.Length; i++)
                     {
                         Debug.Log("capture task camera " + i);
                         Debug.Log("capture task camera has " + room.realizedRoom.cameraPositions.Length + " positions");
-
+                        // load screen
                         game.cameras[0].MoveCamera(i);
                         while(game.cameras[0].www != null) yield return null;
-                        yield return null;
-                        //UnityEngine.Application.CaptureScreenshot(PathOfScreenshot(regionstr, room.name, i));
+                        yield return null; // one extra frame please
+                        // fire!
+                        if(exportScreenshots) UnityEngine.Application.CaptureScreenshot(PathOfScreenshot(regionstr, room.name, i));
+
+                        // palette and colors
+                        mapContent.LogPalette(game.cameras[0].currentPalette);
                     }
                     room.Abstractize();
                     yield return null;
                 }
+
+                Debug.Log("capture task done rooms " + regionstr);
+                File.WriteAllText(PathOfRegion(regionstr) + "metadata.json", Json.Serialize(mapContent));
                 Debug.Log("capture task done with " + regionstr);
+
+                // debug, stop at one region
+                if(oneRegion) break;
             }
             Debug.Log("capture task done!");
             Application.Quit();
