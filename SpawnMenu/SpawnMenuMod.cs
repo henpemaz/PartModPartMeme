ef7b082eb0c2c655aa9120ceed1640647dc2c16a
Henrique Maziero <henpemaz@gmail.com>
2022-02-17 18:55:00 -0300

    Spawn Menu stable

--- SpawnMenu/SpawnMenuMod.cs
+++ SpawnMenu/SpawnMenuMod.cs
@@ -11,6 +11,7 @@ using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using Mono.Cecil.Cil;
 using MonoMod.RuntimeDetour.HookGen;
+using RWCustom;
 
 [assembly: AssemblyTrademark("Henpemaz")]
 
@@ -33,7 +34,7 @@ namespace SpawnMenu
             On.Menu.PauseMenu.Update += PauseMenu_Update;
             On.Menu.PauseMenu.GrafUpdate += PauseMenu_GrafUpdate;
             On.Menu.PauseMenu.ShutDownProcess += PauseMenu_ShutDownProcess;
-            IL.SandboxGameSession.SpawnEntity += SandboxGameSession_SpawnEntity;
+
             new Hook(typeof(ArenaBehaviors.ArenaGameBehavior).GetProperty("room").GetGetMethod(), typeof(SpawnMenuMod).GetMethod("get_room"), this);
         }
 
@@ -41,11 +42,11 @@ namespace SpawnMenu
         {
             orig(self, manager, game);
 
-            if (!game.IsStorySession) return;
+            if (!game.IsStorySession || self.game.cameras[0].room == null) return;
 
             try
             {
-                if (manager.arenaSetup == null)
+                if (manager.arenaSetup == null) // this is required
                 {
                     manager.arenaSetup = new ArenaSetup();
                 }
@@ -56,13 +57,46 @@ namespace SpawnMenu
                 sb.arenaSitting = new ArenaSitting(manager.arenaSetup.GetOrInitiateGameTypeSetup(ArenaSetup.GameTypeID.Sandbox), new MultiplayerUnlocks(manager.rainWorld.progression, new List<string>()));
                 sb.game = game;
 
-                var os = new AbstractCreature(game.world, StaticWorld.GetCreatureTemplate(CreatureTemplate.Type.Overseer), null, new WorldCoordinate() { room = game.world.firstRoomIndex }, new EntityID());
-
+                var os = new AbstractCreature(game.world, StaticWorld.GetCreatureTemplate(CreatureTemplate.Type.Overseer), null, game.Players[0].pos, new EntityID());
+                os.realizedCreature = new Overseer(os, room.world) { room = room, extended = 0f }; // so cursor doesnt flicker
+                if (game.Players[0].realizedCreature != null)
+                {
+                    os.realizedCreature.mainBodyChunk.pos = game.Players[0].realizedCreature.mainBodyChunk.pos;
+                    os.realizedCreature.mainBodyChunk.lastPos = game.Players[0].realizedCreature.mainBodyChunk.lastPos;
+                }
                 room.AddObject(new SandboxOverlayOwner(room, sb, !sb.PlayMode));
+
                 sb.overlay.Initiate(false);
+                for (int l = 0; l < SandboxEditorSelector.Width; l++)
+                {
+                    for (int m = 0; m < SandboxEditorSelector.Height; m++)
+                    {
+                        var btn = sb.overlay.sandboxEditorSelector.buttons[l, m];
+                        // no slugcat and no actions
+                        if ((sb.overlay.sandboxEditorSelector.buttons[l, m] is SandboxEditorSelector.CreatureOrItemButton coib 
+                            && coib.data.itemType == AbstractPhysicalObject.AbstractObjectType.Creature 
+                            && coib.data.critType == CreatureTemplate.Type.Slugcat)
+                            ||
+                            (sb.overlay.sandboxEditorSelector.buttons[l, m] is SandboxEditorSelector.ActionButton ab
+                            && (
+                                ab.action == SandboxEditorSelector.ActionButton.Action.Play ||
+                                ab.action == SandboxEditorSelector.ActionButton.Action.Randomize ||
+                                ab.action == SandboxEditorSelector.ActionButton.Action.ConfigA ||
+                                ab.action == SandboxEditorSelector.ActionButton.Action.ConfigB ||
+                                ab.action == SandboxEditorSelector.ActionButton.Action.ConfigC
+                            )))
+                        {
+                            sb.overlay.sandboxEditorSelector.buttons[l, m] = null;
+                            sb.overlay.sandboxEditorSelector.RemoveSubObject(btn);
+                            btn.RemoveSprites();
+                        }
+                    }
+                }
 
                 sb.editor = new ArenaBehaviors.SandboxEditor(sb);
                 sb.editor.currentConfig = -1;
+
+
                 sb.editor.cursors.Add(new ArenaBehaviors.SandboxEditor.EditCursor(sb.editor, os.abstractAI as OverseerAbstractAI, 0, new Vector2(-1000, -1000)));
                 room.AddObject(sb.editor.cursors[0]);
                 sb.overlay.sandboxEditorSelector.ConnectToEditor(sb.editor);
@@ -72,28 +106,64 @@ namespace SpawnMenu
             }
             catch (Exception e)
             {
-                Debug.LogError("Failed to create sandbox menu");
+                Debug.LogError("SpawnMenuMod failed to create sandbox menu");
                 Debug.LogException(e);
             }
         }
 
         private void PauseMenu_Update(On.Menu.PauseMenu.orig_Update orig, PauseMenu self)
         {
-            orig(self);
-            if (!self.game.IsStorySession || self.game.pauseMenu == null) return;
-            self.game.pauseMenu = null;
-            var room = self.game.cameras[0].room;
-            var overlayowner = room?.updateList.First(o => o is SandboxOverlayOwner) as SandboxOverlayOwner;
-            overlayowner?.Update(false);
+            if (!self.game.IsStorySession || self.game.pauseMenu == null || self.game.cameras[0].room == null) // none of my bisness
+            {
+                orig(self);
+                return;
+            }
 
+            self.game.pauseMenu = null; // several menu thinghies check this and stop working :(
+            // some room thinghies need updating
+            var room = self.game.cameras[0].room;
+            var overlayowner = room.updateList.First(o => o is SandboxOverlayOwner) as SandboxOverlayOwner;
+            overlayowner.Update(false);
+            room.updateList.First(o => o is ArenaBehaviors.SandboxEditor.EditCursor).Update(false);
             foreach(var uad in room.updateList)
             {
                 if(uad is ArenaBehaviors.SandboxEditor.PlacedIcon ico) ico.Update(false);
             }
+            self.game.pauseMenu = self; // done here
 
-            var editCursor = room?.updateList.First(o => o is ArenaBehaviors.SandboxEditor.EditCursor) as ArenaBehaviors.SandboxEditor.EditCursor;
-            editCursor?.Update(false);
-            self.game.pauseMenu = self;
+            // grab processed by our menus not pause menu
+            // if doing anything, pause buttons shut down
+            if ((overlayowner.overlay.sandboxEditorSelector.currentlyVisible
+                    || overlayowner.overlay.sandboxEditorSelector.editor.cursors[0].homeInIcon != null
+                    || overlayowner.overlay.sandboxEditorSelector.editor.cursors[0].dragIcon != null)
+                && self.manager.upcomingProcess == null) // menus freeze input if there's an upcoming process, we use that here to pause the pause menu
+            {
+                self.pressButton = false; //  prevent processing the grab input that brought up the menu                
+                self.manager.upcomingProcess = self.ID;
+                orig(self);
+                self.manager.upcomingProcess = null;
+            }
+            else
+            {
+                orig(self);
+            }
+
+            // fade ctrls on menuing
+            if (self.controlMap != null && (overlayowner.selector.visFac > 0f || overlayowner.selector.lastVisFac > 0f))
+            {
+                // this could be rewritten into a hook for ctrlmap update or something.
+                self.controlMap.fade = Mathf.Clamp01(self.controlMap.fade - overlayowner.selector.visFac);
+                self.controlMap.lastFade = Mathf.Clamp01(self.controlMap.lastFade - overlayowner.selector.lastVisFac);
+                // re calc everything
+                self.controlMap.controlsMap.setAlpha = new float?(self.controlMap.fade);
+                self.controlMap.controlsMap2.setAlpha = new float?(Mathf.Min(self.controlMap.fade, Custom.SCurve(Mathf.InverseLerp(5f, 80f, (float)self.controlMap.counter), 0.8f)));
+                self.controlMap.controlsMap3.setAlpha = new float?(Mathf.Min(self.controlMap.fade, Custom.SCurve(Mathf.InverseLerp(5f, 80f, (float)self.controlMap.counter), 0.8f)) * 0.5f);
+                if (self.controlMap.pickupButtonInstructions != null)
+                {
+                    self.controlMap.pickupFade = Mathf.Clamp01(Custom.SCurve(Mathf.InverseLerp(40f, 120f, (float)self.controlMap.counter), 0.5f) - overlayowner.selector.visFac);
+                    self.controlMap.lastPickupFade = Mathf.Clamp01(Custom.SCurve(Mathf.InverseLerp(40f, 120f, (float)self.controlMap.counter - 1f), 0.5f) - overlayowner.selector.lastVisFac);
+                }
+            }
         }
 
         private void PauseMenu_GrafUpdate(On.Menu.PauseMenu.orig_GrafUpdate orig, PauseMenu self, float timeStacker)
@@ -107,6 +177,7 @@ namespace SpawnMenu
             // timespeed 1 so audio doesnt glitch out
         }
 
+        // close everything and actually spawn stuff
         private void PauseMenu_ShutDownProcess(On.Menu.PauseMenu.orig_ShutDownProcess orig, PauseMenu self)
         {
             try
@@ -118,19 +189,32 @@ namespace SpawnMenu
                     var editor = overlayowner.gameSession.editor;
                     overlayowner.gameSession.PlayMode = true;
                     On.World.GetAbstractRoom_int += World_GetAbstractRoom_int;
-                    room.world.singleRoomWorld = true;
+                    On.AbstractWorldEntity.ctor += AbstractWorldEntity_ctor;
+                    room.world.singleRoomWorld = true; // deer ai checks this, miros probs too
                     foreach (var ico in editor.icons)
                     {
                         if(ico is ArenaBehaviors.SandboxEditor.CreatureOrItemIcon coii)
                         {
                             var data = new ArenaBehaviors.SandboxEditor.PlacedIconData(coii.pos, coii.iconData, coii.ID);
+                            //Debug.Log("SpawnMenu spawning " + coii.iconData.itemType + " " + coii.iconData.critType);
                             overlayowner.gameSession.SpawnEntity(data);
-                            if (room.abstractRoom.entities.Last() is AbstractPhysicalObject apo) apo.RealizeInRoom();
+                            if (room.abstractRoom.entities.Last() is AbstractPhysicalObject apo)
+                            {
+                                //apo.pos.room = room.abstractRoom.index;
+                                //Debug.Log("spawning apo of type " + apo.type);
+                                //Debug.Log("apo tostr " + apo);
+                                //Debug.Log("apo type tostr " + apo.GetType());
+                                //if (apo.type == AbstractPhysicalObject.AbstractObjectType.Creature) Debug.Log("spawning creature type " + (apo as AbstractCreature).creatureTemplate.type);
+                                apo.RealizeInRoom();
+                                //Debug.Log("realized is null ? " + (apo.realizedObject == null));
+                                //if(apo.realizedObject != null) Debug.Log("realized is at " + apo.realizedObject.bodyChunks[0].pos);
+                            }
                         }
                         ico.Fade();
                     }
                     room.world.singleRoomWorld = false;
                     On.World.GetAbstractRoom_int -= World_GetAbstractRoom_int;
+                    On.AbstractWorldEntity.ctor -= AbstractWorldEntity_ctor;
                     overlayowner.Destroy();
                     //room.RemoveObject(overlayowner);
                     overlayowner.overlay.ShutDownProcess();
@@ -142,37 +226,20 @@ namespace SpawnMenu
             finally
             {
                 On.World.GetAbstractRoom_int -= World_GetAbstractRoom_int;
+                On.AbstractWorldEntity.ctor -= AbstractWorldEntity_ctor;
                 orig(self);
             }
+        }
+
+        private void AbstractWorldEntity_ctor(On.AbstractWorldEntity.orig_ctor orig, AbstractWorldEntity self, World world, WorldCoordinate pos, EntityID ID)
+        {
+            pos.room = world.game.cameras[0].room.abstractRoom.index;
+            orig(self, world, pos, ID);
         }
 
         private AbstractRoom World_GetAbstractRoom_int(On.World.orig_GetAbstractRoom_int orig, World self, int room)
         {
             return self.game.cameras[0].room.abstractRoom;
-        }
-
-        private void SandboxGameSession_SpawnEntity(ILContext il)
-        {
-            var c = new ILCursor(il);
-            if (c.TryGotoNext(MoveType.Before,
-                i => i.MatchLdloca(out _),
-                i => i.MatchLdcI4(0),
-                i => i.MatchLdcI4(-1),
-                i => i.MatchLdcI4(-1),
-                i => i.MatchLdcI4(-1),
-                i => i.MatchCall<WorldCoordinate>(".ctor")
-                ))
-            {
-                c.Index++;
-                c.Remove();
-                c.Emit(OpCodes.Ldarg_0);
-                c.EmitDelegate<Func<SandboxGameSession, int>>((s) =>
-                {
-                    if (s.game.IsStorySession) return s.game.cameras[0].room.abstractRoom.index;
-                    return 0;
-                });
-            }
-            else Debug.LogException(new Exception("Couldn't IL-hook SandboxGameSession_SpawnEntity from spawnmenumod")); // deffendisve progrmanig
         }
 
         public delegate Room orig_get_room(ArenaBehaviors.ArenaGameBehavior bhv);
