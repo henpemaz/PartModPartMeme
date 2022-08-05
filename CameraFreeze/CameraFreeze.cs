90adacd0e8b899e3d973731a473598e5fbe2b874
Henrique Maziero <henpemaz@gmail.com>
2022-08-05 09:41:38 -0300

    Proper release of this

--- CameraFreeze/CameraFreeze.cs
+++ CameraFreeze/CameraFreeze.cs
@@ -6,24 +6,22 @@ using BepInEx;
 
 namespace CameraFreeze
 {
-    [BepInPlugin("author.my_mod_id", "SomeModName", "0.1.0")]
+    [BepInPlugin("henpemaz.camerafreeze", "CameraFreeze", "0.1.0")]
     public class CameraFreeze : BaseUnityPlugin
     {
         public void OnEnable()
         {
-            /* This is called when the mod is loaded. */
-
             On.Player.Update += Player_Update;
-
         }
 
         private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
         {
             orig(self, eu);
-            var camera = self.room.game.cameras[0];
+            
             if (self.input[0].mp && !self.input[1].mp)
             {
-                if(camera.followAbstractCreature != null)
+                var camera = self.room.game.cameras[0];
+                if (camera.followAbstractCreature != null)
                 {
                     camera.followAbstractCreature = null;
                 }
